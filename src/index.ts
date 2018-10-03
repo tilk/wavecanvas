407490454aaf117657cd9de1a57b1b610f74ef99
Marek Materzok <tilk@tilk.eu>
2018-10-03 23:30:52 +0200

    Multibit display.

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ import { Vector3vl } from '3vl';
 export { Vector3vl };
 
 export class Waveform {
+    private _present : number;
+
     constructor(private readonly _bits : number, private readonly _data : [number, Vector3vl][]) {
+        if (_data.length == 0) this._present = -1;
+        else this._present = this._data[this._data.length-1][0];
     }
 
     get bits() {
@@ -12,7 +16,8 @@ export class Waveform {
     }
 
     push(t : number, v : Vector3vl) {
-        console.assert(v.bits == this._bits);
+        console.assert(v.bits == this._bits && t > this._present);
+        this._present = t;
         if (this._data.length > 0) {
             const [pt, pv] = this._data[this._data.length-1];
             if (pv.eq(v)) return;
@@ -36,7 +41,15 @@ export class Waveform {
         };
         let first = binsearch(i);
         let last = binsearch(i+n);
-        return this._data.slice(first, last + 1);
+        if (first > 0 && this._data[first][0] > i) first--;
+        if (last < this._data.length-1 && this._data[last][0] < i+n) last++;
+        const s = this._data.slice(first, last + 1);
+        console.log(s[s.length-1][0]);
+        console.log(i+n);
+        console.log(this._present);
+        if (s[s.length-1][0] < i+n && this._present > s[s.length-1][0])
+            s.push([this._present, s[s.length-1][1]]);
+        return s;
     }
 }
 
@@ -50,12 +63,12 @@ export interface WaveCanvasSettings {
 export function drawWaveform(w : Waveform, c : CanvasRenderingContext2D, s : WaveCanvasSettings) {
     const data = w.getRange(s.start, s.span);
     const zdata = data.map((e, i) => [e, data[i+1]]);
-    zdata[zdata.length-1][1] = zdata[zdata.length-1][0];
+    zdata.pop();
     c.clearRect(0, 0, c.canvas.width, c.canvas.height);
     const t2x = (t) => (t - s.start) / s.span * c.canvas.width;
     const xy = 0.5 * c.canvas.height;
-    const hy = 0.05 * c.canvas.height;
-    const ly = 0.95 * c.canvas.height;
+    const hy = 0.1 * c.canvas.height;
+    const ly = 0.9 * c.canvas.height;
     const b2y = (b) => [ly, xy, hy][b+1];
     const b2c = (b) => s.bitColors[b+1];
     const w2c = (w) => s.bitColors[!w.isDefined ? 1 : w.isLow ? 0 : w.isHigh ? 2 : 3];
@@ -63,15 +76,16 @@ export function drawWaveform(w : Waveform, c : CanvasRenderingContext2D, s : Wav
     grad.addColorStop(0, s.bitColors[0]);
     grad.addColorStop(0.5, s.bitColors[1]);
     grad.addColorStop(1, s.bitColors[2]);
+    c.lineWidth = 0.5;
     for (const t of Array(s.span).keys()) {
         const x = t2x(t + s.start);
         c.beginPath();
-        c.lineWidth = 0.5;
         c.strokeStyle = 'gray';
         c.moveTo(x, 0);
         c.lineTo(x, c.canvas.height);
         c.stroke();
     }
+    c.lineWidth = 2;
     for (const [[at, av], [bt, bv]] of zdata) {
         const ax = t2x(at), bx = t2x(bt);
         if (w.bits == 1) {
@@ -79,7 +93,6 @@ export function drawWaveform(w : Waveform, c : CanvasRenderingContext2D, s : Wav
             const ay = b2y(ab), by = b2y(bb);
             const ac = b2c(ab), bc = b2c(bb);
             const gap = Math.min(Math.abs(ay-by) * s.gapScale, c.canvas.width / s.span / 2);
-            c.lineWidth = 2;
             c.beginPath();
             c.strokeStyle = grad;
             c.moveTo(bx - gap, ay);
@@ -100,35 +113,43 @@ export function drawWaveform(w : Waveform, c : CanvasRenderingContext2D, s : Wav
                 c.moveTo(ax, xy);
                 c.lineTo(bx, xy);
                 c.stroke();
+            } else if (av.eq(bv)) {
+                c.strokeStyle = bc;
+                c.beginPath();
+                c.moveTo(ax, hy);
+                c.lineTo(bx, hy);
+                c.stroke();
+                c.beginPath();
+                c.moveTo(ax, ly);
+                c.lineTo(bx, ly);
+                c.stroke();
             } else {
-                if (ad) {
-                    c.beginPath();
-                    c.strokeStyle = ac;
-                    c.moveTo(ax, hy);
-                    c.lineTo(ax + gap/2, xy);
-                    c.lineTo(ax, ly);
-                    c.stroke();
-                } else {
-                    c.beginPath();
-                    c.strokeStyle = ac;
-                    c.moveTo(ax, xy);
-                    c.lineTo(ax + gap/2, xy);
-                    c.stroke();
-                }
+                c.strokeStyle = bc;
                 if (bd) {
                     c.beginPath();
-                    c.strokeStyle = bc;
                     c.moveTo(bx, hy);
-                    c.lineTo(ax + gap, hy);
-                    c.lineTo(ax + gap/2, xy);
-                    c.lineTo(ax + gap, ly);
+                    c.lineTo(bx - gap/2, xy);
                     c.lineTo(bx, ly);
                     c.stroke();
                 } else {
                     c.beginPath();
-                    c.strokeStyle = bc;
-                    c.moveTo(ax + gap/2, xy);
                     c.moveTo(bx, xy);
+                    c.lineTo(bx - gap/2, xy);
+                    c.stroke();
+                }
+                c.strokeStyle = ac;
+                if (ad) {
+                    c.beginPath();
+                    c.moveTo(ax, hy);
+                    c.lineTo(bx - gap, hy);
+                    c.lineTo(bx - gap/2, xy);
+                    c.lineTo(bx - gap, ly);
+                    c.lineTo(ax, ly);
+                    c.stroke();
+                } else {
+                    c.beginPath();
+                    c.moveTo(ax, xy);
+                    c.lineTo(bx - gap/2, xy);
                     c.stroke();
                 }
             }
